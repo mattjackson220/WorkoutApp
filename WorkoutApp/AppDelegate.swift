baf06716ac56921bceb6782c502984c38daf5cac
mattjackson220 <mjackso8@alumni.nd.edu>
2021-06-25 12:29:23 -0600

    disabled 3rd party keyboard inputs

--- WorkoutApp/AppDelegate.swift
+++ WorkoutApp/AppDelegate.swift
@@ -40,6 +40,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+    func application(_ application: UIApplication, shouldAllowExtensionPointIdentifier extensionPointIdentifier: UIApplicationExtensionPointIdentifier) -> Bool {
+        return extensionPointIdentifier != UIApplicationExtensionPointIdentifier.keyboard 
+    }
 
 
 }
